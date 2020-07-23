85036f5cac18fdc51a8f4c08573ea8cdb5b20f10
thegroovetrain <ericthegroovetrain@gmail.com>
2020-07-23 19:10:14 -0400

    Revert "production test fix"
    This reverts commit 0e3d7e87b8222dcbd047a27f36327c5d26d52d2e.

--- knexfile.js
+++ knexfile.js
@@ -31,23 +31,5 @@ module.exports = {
   			conn.run("PRAGMA foreign_keys = ON", done)
   		},
   	},
-  },
-  production: {
-    client: "sqlite3",
-  	useNullAsDefault: true,
-  	connection: {
-  		filename: "./database/production.db3",
-  	},
-  	migrations: {
-  		directory: "./database/migrations",
-  	},
-  	seeds: {
-  		directory: "./database/seeds",
-  	},
-  	pool: {
-  		afterCreate: (conn, done) => {
-  			conn.run("PRAGMA foreign_keys = ON", done)
-  		},
-  	},
   }
 };
